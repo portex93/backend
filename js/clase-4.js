26afa2ec5a4f18f3de9bbc3073f5870b61300f63
ericdiaz <eric_205_93@hotmail.com>
2023-04-04 11:57:20 -0300

    agregue addproduct

--- js/clase-4.js
+++ js/clase-4.js
@@ -4,18 +4,31 @@ class ProductManager {
     }
 
     addProducts (Products) {
-        
+        if(this.Products.find(producto => producto.code == Products.code)){
+            console.log ("Producto existente")
+        } else {
+
+        }
     }
 }
 
 class Products {
-    constructor (tittle, description, price, thumbnail, code, stock) {
+    constructor (tittle, description, price, stock, code) {
     this.tittle = tittle
     this.description = description
     this.price = price
     this.code = code
     this.stock = stock
-    this.thumbnail = thumbnail
+    this.id = Products.incrementID()
+    }
+
+    static incrementID () {
+        if (this.idIncrement){
+            this.idIncrement++
+        } else {
+            this.idIncrement = 1
+        }
+        return this.idIncrement
     }
     
 }
@@ -24,12 +37,12 @@ class Products {
 
 
 
-let escolar = new Products ( "libreta", "rayado", "$150", 1,"10");
-let escolar1 = new Products ("lapicera", "azul", "$80", 2, "15")
-let escolar2 = new Products ("goma de borrar", "blanca", "$50", 3, "20")
-let escolar3 = new Products ("lapiz", "negro", "$70", 4, "30")
+let escolar = new Products ( "libreta", "rayado", "$150", "10", "L450",);
+let escolar1 = new Products ("lapicera", "azul", "$80", "15", "LAP200", )
+let escolar2 = new Products ("goma de borrar", "blanca", "$50", "20", "GDB800", )
+let escolar3 = new Products ("lapiz", "negro", "$70", "30", "LPZ250", )
 
-console.log()
-
-
-
+console.log(escolar)
+console.log(escolar1)
+console.log(escolar2)
+console.log(escolar3) 
